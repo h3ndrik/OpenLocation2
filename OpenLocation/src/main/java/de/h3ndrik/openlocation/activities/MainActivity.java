934f386cfc016a45633ad6243619f211591e471b
meisterfuu <info@meister-fuu.de>
2013-12-03 17:15:08 +0100

    Added activities package

--- OpenLocation/src/main/java/de/h3ndrik/openlocation/activities/MainActivity.java
+++ OpenLocation/src/main/java/de/h3ndrik/openlocation/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package de.h3ndrik.openlocation;
+package de.h3ndrik.openlocation.activities;
 
 import android.app.Activity;
 import android.app.ActionBar;
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+
+import de.h3ndrik.openlocation.R;
 
 public class MainActivity extends Activity {
 
