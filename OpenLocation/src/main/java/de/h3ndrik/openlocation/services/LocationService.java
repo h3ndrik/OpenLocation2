a6bbc94c74d204e2339e06f444457207b1370f59
Hendrik Langer <dev@h3ndrik.de>
2013-12-03 17:43:46 +0100

    Add Service stubs

---
+++ OpenLocation/src/main/java/de/h3ndrik/openlocation/services/LocationService.java
@@ -0,0 +1,14 @@
+package de.h3ndrik.openlocation.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by h3ndrik on 03.12.13.
+ */
+public class LocationService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
